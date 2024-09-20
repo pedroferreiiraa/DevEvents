ed09bce6871d0aea3c407cb7d9d6c737a045eaaa
Pedro <pdro5k@outlook.com>
2024-09-20 11:18:18 -0300

    Alterando dados de contato no swagger

--- Controllers/EventosController.cs
+++ Controllers/EventosController.cs
@@ -16,7 +16,12 @@
                 _contexto = contexto;
             }    
         
-            // GET api/eventos
+            /// <summary>
+            /// Listagem de eventos
+            /// </summary>
+            /// <param name="id"></param>
+            /// <response code="200">Sucesso</response>
+            /// <returns>Lista de eventos</returns>
             [HttpGet]
             public IActionResult GetAll()
             {
@@ -24,7 +29,13 @@
                 return Ok(eventos);
             }
 
-            // GET api/eventos/1
+            /// <summary>
+            /// Detalhes de Evento
+            /// </summary>
+            /// <param name="id">Identificador do Evento</param>
+            /// <response code="404">Não encontrado </response>
+            /// <response code="200">Sucesso </response>
+            /// <returns>Detalhes de Evento</returns>
             [HttpGet("{id}")]
             public IActionResult GetById(int id)
             {
@@ -36,8 +47,24 @@
                 return Ok(evento);
             }
             
-            // POST api/eventos
+            /// <summary>
+            /// Cadastro de Evento
+            /// </summary>
+            /// <remarks>
+            ///  {
+            ///     "titulo": "Jornada .NET",
+            ///     "descricao": "Evento para destravar carreiras e acelerar resultados",
+            ///     "dataInicio": "2024-09-20T13:35:55.751Z",
+            ///     "dataFim": "2024-09-24T13:35:55.751Z",
+            ///     "organizador": "Luis Dev"
+            ///  }
+            /// </remarks>
+            /// <param name="evento">Dados do Evento</param>
+            /// <returns>Evento recém criado</returns>
+            /// <response code="201">Sucesso</response>
             [HttpPost]
+            [ProducesResponseType(StatusCodes.Status201Created)]
+            
             public IActionResult Post(Evento evento)
             {
                 _contexto.Eventos.Add(evento);
@@ -47,8 +74,27 @@
                     );
             }
             
-            // PUT api/eventos/1
+            /// <summary>
+            /// Atualização de Evento
+            /// </summary>
+            /// <remarks>
+            ///  {
+            ///     "id": 1,
+            ///     "titulo": "Jornada .NET",
+            ///     "descricao": "Evento para destravar carreiras e acelerar resultados",
+            ///     "dataInicio": "2024-09-20T13:35:55.751Z",
+            ///     "dataFim": "2024-09-24T13:35:55.751Z",
+            ///     "organizador": "Luis Dev"
+            ///  }
+            /// </remarks>
+            /// <param name="id">Identificador do Evento></param>
+            /// <param name="evento">Dados do Evento</param>
+            /// <returns>Evento recém criado</returns>
+            /// <response code="204">Sucesso</response>
+            /// <response code="404">Não encontrado</response>
             [HttpPut("{id}")]
+            [ProducesResponseType(StatusCodes.Status204NoContent)]
+            [ProducesResponseType(StatusCodes.Status404NotFound)]
             public IActionResult Put(int id, Evento evento)
             {
                 var eventoExistente = _contexto.Eventos.SingleOrDefault(e => e.Id == id);
@@ -65,8 +111,17 @@
                 return NoContent();
             }
             
-            // DELETE api/eventos/1
+            /// <summary>
+            /// Remoção de Evento
+            /// </summary>
+            /// <param name="id">Identificador do Evento</param>
+            /// <response code="404">Não encontrado </response>
+            /// <response code="200">Sucesso </response>
+            /// <returns></returns>
             [HttpDelete("{id}")]
+            [ProducesResponseType(StatusCodes.Status204NoContent)]
+            [ProducesResponseType(StatusCodes.Status404NotFound)]
+
             public IActionResult Delete(int id)
             {
                 var evento = _contexto.Eventos.SingleOrDefault(e => e.Id == id);
--- Program.cs
+++ Program.cs
@@ -1,17 +1,53 @@
 using DevEvents.API.Entities;
 using DevEvents.API.Persistence;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // builder.Services.AddSingleton<EventosDbContext>();
-// builder.Services.AddDbContext<EventosDbContext>(
-//     o => o.UseInMemoryDatabase("DevEventosDb")
-// );
-
-builder.Services.AddDbContext<EventosDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+builder.Services.AddDbContext<EventosDbContext>(
+    o => o.UseInMemoryDatabase("DevEventosDb")
 );
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("PermitirTudo", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "DevEvents API",
+        Version = "v1",
+        Contact = new OpenApiContact
+        {
+            Name = "Pedro",
+            Email = "pdro5k@outlook.com",
+            Url = new Uri("https://github.com/pedroferreiiraa")
+        }
+    });
+    
+    
+    // Pega o caminho do arquivo XML que foi gerado
+    var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+    // Inclui o arquivo XML para o Swagger
+    options.IncludeXmlComments(xmlPath);
+});
+
+// builder.Services.AddDbContext<EventosDbContext>(options =>
+//     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+// );
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
